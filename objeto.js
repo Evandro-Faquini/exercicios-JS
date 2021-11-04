4d279d3abf150108d5b15ba7534954df2262d45e
Evandro Faquini <83242940+Evandro-Faquini@users.noreply.github.com>
2021-11-04 13:03:54 -0300

    Add files via upload

---
+++ objeto.js
@@ -0,0 +1,12 @@
+
+function externo(){
+
+    let pessoa = {
+    Nome         :"Evandro",
+    Sobrenome    :"Faquini",
+    Departamento : "Ti"
+    }
+    document.getElementById("local").innerHTML = "Olá meu nome é " + pessoa.Nome + " , trabalho no " + pessoa.Departamento + " , posso ajudar?";
+
+
+}
